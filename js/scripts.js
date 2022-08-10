20a956e3165ddeca18475eb475d23ff5c5f64f7a
Skylan Lew <skylanslew@gmail.com>
2022-08-10 15:20:55 -0700

    completed hide/show javascript, added center style
    Co-authored-by: Trevor Hunter <trevorh8700@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,17 @@ window.onload = function(){
   form.onsubmit = function(event){
     event.preventDefault();
 
+    const turtle = document.getElementById("turtle");
+    const monkey = document.getElementById("monkey");
+    const snake = document.getElementById("snake");
+    const invalid = document.getElementById("invalid");
+
+    turtle.setAttribute("class", "hidden");
+    monkey.setAttribute("class", "hidden");
+    snake.setAttribute("class", "hidden");
+    invalid.setAttribute("class", "hidden");
+
+
     const userRequest = document.getElementById("userRequest").value.toLowerCase();
     console.log("User requested " + userRequest)
 
@@ -12,14 +23,20 @@ window.onload = function(){
 };
 
 
+
 function checkRequest(userRequest) {
   if (userRequest === "turtle") {
     console.log("Take me to turtle");
+    turtle.removeAttribute("class");
   } else if (userRequest === "monkey") {
     console.log("Take me to monkey");
+    monkey.removeAttribute("class");
   } else if (userRequest === "snake") {
     console.log("Take me to snake");
+    snake.removeAttribute("class");
   } else {
+    invalid.removeAttribute("class");
     console.log("Invalid request");
+
   };
 };
